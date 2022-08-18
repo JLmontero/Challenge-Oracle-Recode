81270c03559aa1e0191960e63e5752d7cb05e501
JLmontero <jmontero81@hotmail.com>
2022-08-17 21:21:13 -0400

    Encriptado de texto

---
+++ Encriptador.js
@@ -0,0 +1,60 @@
+/**
+ * Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+ */
+
+//Definiendo la funcion de encritado...
+
+function encriptar()
+{
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    //console.log(texto);
+
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgEnc").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+//Definiendo la funcion de desencritado...
+
+function desencriptar()
+{
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgEnc").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+}
+//Definiendo la funcion de copiado...
+
+function textCopy()
+{
+    var srtcont = document.querySelector("#texto2");
+    srtcont.select();
+    document.execCommand("copy");
+    alert("Se copió correctamente.");
+    
+}
+
